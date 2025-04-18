aa5b3989e1e84db4481269fb3604ba912de8b821
André Landim Pereira <46598452+alandim@users.noreply.github.com>
2025-04-18 14:35:02 -0300

    Create repete_string.py

---
+++ repete_string.py
@@ -0,0 +1,9 @@
+# Solicita a string e o número de vezes
+texto = input("Digite uma string: ")
+quantidade = int(input("Digite um número inteiro: "))
+
+# Repete a string
+resultado = texto * quantidade
+
+# Exibe o resultado
+print(f"Resultado: {resultado}")
